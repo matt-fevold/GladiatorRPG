a5b1fe63fa44dd621f94f6207326b959d3e5133d
Matthew Fevold <matt.fevold@gmail.com>
2017-12-01 15:24:50 -0600

    loads of goodies
    +Handler base class
    I can now call handler.draw to draw whichever WidgetHandler I want (right now just have button & graphic handler
    
    +GraphicHandler,  made it work similar to buttonHandler
    
    +ButtonHandler, made it a bit nicer and added screen property (which is a pygame surface
    
    +Button, Graphic, abstracted out the screen from the class since it didn't make sense to have that apart of the class, it's still a parameter to draw_widget due to that is needed to actually draw, but now all of that is handled in the Handler.... shocker.

--- main.py
+++ main.py
@@ -21,8 +21,12 @@ clock = pygame.time.Clock()
 class Scene:
     # for generic scene creation.
     def __init__(self):
+        # Add UI element handlers here as I need them
         self.ButtonManager = ButtonHandler()
         self.GraphicManager = GraphicHandler()
+
+    def draw_scene(self):
+        pass
 
 
 class Combat(Scene):
@@ -30,37 +34,62 @@ class Combat(Scene):
         super().__init__()
 
 
+class Handler:
+    def __init__(self):
+        raise NotImplementedError
+
+    def draw(self):
+        raise NotImplementedError
+
+
 class Graphic:
-    pass
+    def __init__(self, image_name, image_x, image_y):
+
+        self.image_x = image_x  # the x/y location on the screen passed
+        self.image_y = image_y
+        self.image = pygame.image.load('Assets/' + image_name)
+
+        # self.draw_graphic()
+
+    def draw_graphic(self, screen):
+        screen.blit(self.image, [self.image_x, self.image_y])
 
 
-class GraphicHandler:
+class GraphicHandler(Handler):
     """handle a group of graphics
     """
-    def __init__(self):
+    def __init__(self, screen):
         self.graphic_list = []
+        self.screen = screen
 
-    def add_graphic(self):
-        pass  # TODO
+    def add_graphic(self, image):
+        self.graphic_list.append(image)
+
+    def draw_graphics(self):
+        [g.draw_graphic(self.screen) for g in self.graphic_list]
+
+    def draw(self):
+        self.draw_graphics()
 
 
 class Button:
     # all a button is is a rectangle, some text, and an action on left click
 
-    def __init__(self, x_1, y_1, x_2, y_2, text, color):
+    def __init__(self, screen, x_1, y_1, x_2, y_2, text, color):
         self.name = text
+        self.color = color
+        self.screen = screen
 
         self.x_1 = x_1
         self.x_2 = x_2
         self.y_1 = y_1
         self.y_2 = y_2
-        self.color = color
 
-        self.draw_button()
+        # self.draw_button()
 
-    def draw_button(self):
-        pygame.draw.rect(gameDisplay, self.color, (self.x_1, self.y_1,
-                                                   self.x_2, self.y_2))
+    def draw_button(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x_1, self.y_1,
+                                              self.x_2, self.y_2))
 
         self.draw_text()
 
@@ -71,6 +100,7 @@ class Button:
 
         gameDisplay.blit(label, (center_x, center_y))
 
+    # TODO make clicking better, seems only clicks work on left side.
     def within(self, mouse_click_x, mouse_click_y):
         if self.x_1 <= mouse_click_x <= self.x_2:
             if self.y_1 < mouse_click_y <= self.y_2:
@@ -79,26 +109,46 @@ class Button:
 
     def action(self):
         self.color = button_clicked_color
-        self.draw_button()
+        self.draw_button(gameDisplay)
         print(self.name, " is pressed")
 
 
-class ButtonHandler:
+class ButtonHandler(Handler):
     # place all buttons here,
-    def __init__(self):
+    def __init__(self, screen):
         self.button_list = []
+        self.screen = screen
 
-    def add_button(self, x_1, y_1, x_2, y_2, text, button_color):
-        self.button_list.append(Button(x_1, y_1, x_2, y_2, text, button_color))
+    def add_button(self, butt):
+        # self.button_list.append(Button(x_1, y_1, x_2, y_2, text, button_color))  # can't believe i wrote this line
+                                                                            # keeping until new method works
+        self.button_list.append(butt)
+
+    def draw_buttons(self):
+        # draw all the buttons
+        [b.draw_button(self.screen) for b in self.button_list]
+
+    def draw(self):
+        self.draw_buttons()
 
 
 gameDisplay.fill(white)
-button_handler = ButtonHandler()
+button_handler = ButtonHandler(gameDisplay)
 #  button_handler.add_button(game_x * .15, game_y * .8, game_x * .2, game_y * .9, "asdf", blue)
-button_handler.add_button(5, game_y * 0.8, 100, game_y * 0.85, "New Game", blue)
-button_handler.add_button(100, game_y * 0.8, 171, game_y * 0.85, "Begin Combat", green)
-imageTest = pygame.image.load('Assets/Defualt_Image.png')
+button1 = Button(gameDisplay, 5, game_y * 0.8, 100, game_y * 0.85, "New Game", blue)
+button2 = Button(gameDisplay, 100, game_y * 0.8, 171, game_y * 0.85, "Begin Combat", green)
 
+button_handler.add_button(button1)
+button_handler.add_button(button2)
+# imageTest = pygame.image.load('Assets/Defualt_Image.png')
+
+imageTest = Graphic('Defualt_Image.png', 50, 50)
+
+button_handler.draw_buttons()
+
+graphic_handler = GraphicHandler(gameDisplay)
+graphic_handler.add_graphic(imageTest)
+graphic_handler.draw_graphics()
 
 exit_game = False
 
@@ -114,12 +164,14 @@ while not exit_game:
                 mouse_x, mouse_y = event.pos
 
                 # check button area (for now confined to bottom 20% of screen
-                if True: #mouse_y > game_y * 0.8: #TODO: efficiency spot
+                if True:  # mouse_y > game_y * 0.8;  # TODO: efficiency spot
                     for button in button_handler.button_list:
                         if button.within(mouse_x, mouse_y):
                             button.action()
 
-    gameDisplay.blit(imageTest, [50, 50])
+    # gameDisplay.blit(imageTest, [50, 50])
+    #imageTest.draw_graphic(gameDisplay)
+
 
     pygame.display.update()
     clock.tick(60)
